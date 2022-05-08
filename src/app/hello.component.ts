3164d55c33dda881d62172800d397570002808f1
guptasanskriti445 <guptasanskriti445@gmail.com>
2022-05-08 10:37:35 +0530

    hello component changes

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,17 +1,11 @@
-import { Component, Input, Inject, OnInit, OnDestroy } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'hello',
   template: `<h1>Hello {{name}}!</h1>`,
   styles: [`h1 { font-family: Lato; }`],
 })
-export class HelloComponent implements OnInit, OnDestroy {
+export class HelloComponent implements OnInit {
   @Input() name: string;
-  constructor(@Inject(DOCUMENT) private _document: any) {}
-  ngOnInit(): void {
-    this._document.body.classList.add('bg-color');
-  }
-  ngOnDestroy() {
-    this._document.body.classList.remove('bg-color');
-  }
+  constructor() {}
+  ngOnInit(): void {}
 }
